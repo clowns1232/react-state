09d185969514a26fe38ac0b42d29323fd5d9483f
황태현 <clowns1232@gmail.com>
2023-07-06 17:52:19 +0900

    api: 추가 1. tasks : 상태변경, 추가, 조회 2. isDarkMode: 조회, 상태변경

---
+++ src/api/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+const api = axios.create({
+  baseURL: "/",
+});
+
+export const getServerTasks = () => api.get(`tasks`).then((res) => res.data);
+export const postServerTasks = (label: string) =>
+  api
+    .post(`tasks`, {
+      label: label,
+      id: uuidv4(),
+      complete: false,
+    })
+    .then((res) => res.data);
+export const changeServerTasks = (id: string, complete: boolean) =>
+  api.patch(`tasks/${id}`, {
+    complete: !complete,
+  });
+
+export const getServerDarkMode = () =>
+  api.get(`isDarkMode`).then((res) => res.data);
+export const putServerDarkMode = (thisDarkMode?: boolean) =>
+  api.put("isDarkMode", {
+    isDarkMode: !thisDarkMode,
+  });
