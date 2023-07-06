fdd9547f3d3ffb8cfa81da51c77c0d239bfd14db
황태현 <clowns1232@gmail.com>
2023-07-06 17:56:31 +0900

    temp: swr: get

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Task } from "./Task";
+import useSWR from "swr";
+import { getServerTasks } from "../api/api";
 
 export const Tasks: React.FC = () => {
+  const { data } = useSWR("/tasks", getServerTasks);
+  console.log(data);
+
   return (
     <div>
       <Task id={0} />
